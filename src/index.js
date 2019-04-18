a6b50f2b1e64de5188ffe8f0df18f410a912bf0b
Thiago Spindola <thiago.spindola@aapvr.com.br>
2019-04-18 18:00:08 -0300

    continuacao

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-        { /* No caso de um componente dentro de outro, usar o nome do componente como uma TAG JSX. */ }
-        <CommentDetail author="Sam"/>
-        <CommentDetail author="Alex"/>
-        <CommentDetail author="Jane"/>
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        {/* No caso de um componente dentro de outro, usar o nome do componente como uma TAG JSX. */}
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          postComment="Nice!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          postComment="Very well!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          postComment="Fuck you!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
